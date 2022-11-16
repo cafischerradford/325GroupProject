bbe5f452b836da4780c9763de06cdec664afc7b6
Blake Conner <biconner@radford.edu>
2022-11-16 00:19:46 -0500

    added category descriptions

--- resources/views/drinks.blade.php
+++ resources/views/drinks.blade.php
@@ -3,7 +3,7 @@
 <x-head title="Grocer | Drinks"/>
 <body class="bg-gray-200">
     <x-header active="drinks" :count="$count"/>
-    <x-banner title="Drinks" description="Our drinks come from local, award winning, brewers, wineries, and distilleries. We chose our sources because of their meticulous care for each drink, so we can ensure our customers recieve the highest quality beverages with every bottle." />
+    <x-banner title="Drinks" description="Our drinks come from local, award winning, breweries, wineries, and distilleries. We chose our sources because of their meticulous care for each drink, so we can ensure our customers recieve the highest quality beverages with every bottle." />
     <x-product-list :products="$products"/>
 </body>
 </html>
--- resources/views/fruits_vegs.blade.php
+++ resources/views/fruits_vegs.blade.php
@@ -3,7 +3,7 @@
 <x-head title="Grocer | Fruits & Vegetables"/>
 <body class="bg-gray-200">
     <x-header active="fruits_vegetables" :count="$count"/>
-    <x-banner title="Fruits & Vegetables" description="Our fruits and vegetables are sourced from American farmers who use only the best organic farming practices. Each of our fruit and vegtable products are packaged at the peak of ripeness and delivered to our stores within hours. You can be confident you are recieving the best american produce avalable when shopping with Grocer." />
+    <x-banner title="Fruits & Vegetables" description="Our fruits and vegetables are sourced from American farmers who use only the best organic farming practices. Each of our fruit and vegtable products are packaged at the peak of ripeness and delivered to our stores within hours. You can be confident you are recieving the best American produce avalable when shopping with Grocer." />
     <x-product-list :products="$products" /> 
 </body>
 </html>
